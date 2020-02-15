c9cad3d72b84ab32a76df100a77453fcb383706f
Lean Junio <leanjunio@live.com>
2020-02-15 09:08:54 -0500

    start experiences by start date

--- src/routes/experienceRoute.js
+++ src/routes/experienceRoute.js
@@ -5,7 +5,8 @@ const router = Router();
 router.get('/', async (req, res, next) => {
   let experiences;
   try {
-    experiences = await Experience.find({});
+    experiences = await Experience.find({}).lean();
+    experiences = experiences.sort((a, b) => b.start - a.start);
   } catch (error) {
     return next(error);
   }
