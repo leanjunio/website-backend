dc10d7d070041b6bdd29f96760f577e82a919426
Lean Junio <leanjunio@live.com>
2020-02-15 00:07:39 -0500

    Add new fields

--- src/models/experience.js
+++ src/models/experience.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const experienceSchema = mongoose.Schema({
   companyName: String,
   technology: [String],
-  desc: [String],
+  position: String,
+  location: String,
+  desc: String,
+  duration: String,
 });
 
 module.exports = mongoose.model('experience', experienceSchema);
--- src/models/info.js
+++ src/models/info.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const infoSchema = mongoose.Schema({
   summary: String,
+  header: String,
+  aboutMe: String,
 });
 
 module.exports = mongoose.model('info', infoSchema);
