878486f051c5e5c1551702ff2010f0a0d8b19bd4
Lean Junio <leanjunio@live.com>
2020-02-19 11:02:50 -0500

    update to serve index.html

--- src/app.js
+++ src/app.js
@@ -3,12 +3,17 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const connectDB = require('./middleware/connectDB');
 const configRoutes = require('./routes');
+const path = require('path');
+
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(connectDB);
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'dist')));
 
 configRoutes(app);
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,14 @@ const infoRoutes = require('./infoRoutes');
 const experienceRoutes = require('./experienceRoute');
 const projectRoutes = require('./projectRoutes');
 
+const path = require('path');
+
 module.exports = app => {
   app.use('/info', infoRoutes);
   app.use('/experience', experienceRoutes);
   app.use('/project', projectRoutes);
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+  });
 };
